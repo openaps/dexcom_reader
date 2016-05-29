e832319c2d4712c29f01af6b019faba723dec1d7
Ben West <bewest@gmail.com>
2016-05-29 12:30:36 -0700

    prepare pkg_resources AGAIN

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(name='dexcom_reader',
         'Topic :: Software Development :: Libraries'
     ],
     package_data={
-      'dexcom_reader': ['dexcom_reader/etc/udev/rules.d/*']
+      'dexcom_reader': ['etc/udev/rules.d/*']
     },
     data_files = [
       ('/etc/udev/rules.d', ['dexcom_reader/etc/udev/rules.d/80-dexcom.rules'] ),
